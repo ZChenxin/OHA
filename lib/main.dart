64db19385bc1160201b9300126a3f2c831d6581b
Zhao Wei Liew <zhaoweiliew@gmail.com>
2017-11-20 00:48:26 +0800

    Set up initial app/home title and color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Hwa Chong',
       theme: new ThemeData(
         // This is the theme of your application.
         //
@@ -19,9 +19,9 @@ class MyApp extends StatelessWidget {
         // "hot reload" (press "r" in the console where you ran "flutter run",
         // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
         // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(title: 'Hwa Chong'),
     );
   }
 }
